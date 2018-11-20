bd1d515fee53a77b2147488b9313f48288c027a4
KAI-PC <t105360045@ntut.org.tw>
2018-11-20 13:51:38 +0800

    RailODFare finish

---
+++ app/src/main/java/com/example/wesdx/a107_ntut_applicationsoftwaredesign_final/RailODFare.java
@@ -0,0 +1,28 @@
+package com.example.wesdx.a107_ntut_applicationsoftwaredesign_final;
+/* 20181120 1351 完成*/
+public class RailODFare {
+    public String OriginStationID;
+    OriginStationNameC OriginStationName;
+    public String DestinationStationID;
+    DestinationStationNameC DestinationStationName;
+    public String Direction;
+    FaresC Fares;
+    public String UpdateTime;
+}
+class OriginStationNameC{
+    public String Zh_tw;
+    public String En;
+}
+class DestinationStationNameC{
+    public String Zh_tw;
+    public String En;
+}
+class FaresC{
+    FareC Fare;
+}
+
+class  FareC{
+    public String TicketType;
+    public String Price;
+}
+
