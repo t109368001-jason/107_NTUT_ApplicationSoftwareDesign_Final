e457b274835f757983a705653ff4991e23bfc133
KAI-PC <t105360045@ntut.org.tw>
2018-11-20 14:05:19 +0800

    RailGeneralTrainInfo finish

---
+++ app/src/main/java/com/example/wesdx/a107_ntut_applicationsoftwaredesign_final/RailGeneralTrainInfo.java
@@ -0,0 +1,21 @@
+package com.example.wesdx.a107_ntut_applicationsoftwaredesign_final;
+/* 20181120 1404 完成*/
+public class RailGeneralTrainInfo {
+    public String TrainNo;
+    public String Direction;
+    public String StartingStationID;
+    StartingStationNameC StartingStationName;//前面宣告過了
+    public  String EndingStationID;
+    EndingStationNameC EndingStationName; //前面宣告過了
+    public String TrainTypeID;
+    public String TrainTypeCode;
+    TrainTypeNameC TrainTypeName; //前面宣告過了
+    public String TripLine;
+    public String WheelchairFlag;
+    public String PackageServiceFlag;
+    public String DiningFlag;
+    public String BikeFlag;
+    public String BreastFeedingFlag;
+    public String DailyFlag;
+    NoteC Note; //前面宣告過了
+}
