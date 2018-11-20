7e385956b48be6aa76d131218d09bd7cc9a4b929
KAI-PC <t105360045@ntut.org.tw>
2018-11-20 13:04:36 +0800

    RailGeneralTimetable

---
+++ app/src/main/java/com/example/wesdx/a107_ntut_applicationsoftwaredesign_final/RailGeneralTimetable.java
@@ -0,0 +1,25 @@
+package com.example.wesdx.a107_ntut_applicationsoftwaredesign_final;
+
+public class RailGeneralTimetable
+{
+    public String UpdateTime;
+    public GeneralTimetableC GeneralTimetable;
+
+
+}
+
+class GeneralTimetableC {
+    GeneralTrainInfoC GeneralTrainInfo;
+    StopTimesC StopTimes;
+    ServiceDayC ServiceDay;
+
+}
+class GeneralTrainInfoC{
+
+}
+class StopTimesC{
+
+}
+class ServiceDayC{
+
+}
--- app/src/main/java/com/example/wesdx/a107_ntut_applicationsoftwaredesign_final/RailStation.java
+++ app/src/main/java/com/example/wesdx/a107_ntut_applicationsoftwaredesign_final/RailStation.java
@@ -8,16 +8,6 @@ package com.example.wesdx.a107_ntut_applicationsoftwaredesign_final;
  */
 public class RailStation {
 
-	class StationNameC
-	{
-		String Zh_tw;
-		String En;
-	}
-	class StationPositionC
-	{
-		float PositionLon;
-		float PositionLat;
-	}
     public String StationID;
 	public StationNameC StationName;
 	public StationPositionC StationPosition;
@@ -25,52 +15,15 @@ public class RailStation {
     public String StationPhone;
     public String StationClass;
 
-	public String getStationID() {
-		return StationID;
-	}
-	public void setStationID(String stationID) {
-		StationID = stationID;
-	}
-	public String getStationNameZh() {
-		return StationName.Zh_tw;
-	}
-	public void setStationNameZh(String stationNameZh) {
-		StationName.Zh_tw = stationNameZh;
-	}
-	public String getStationNameEn() {
-		return StationName.En;
-	}
-	public void setStationNameEn(String stationNameEn) {
-		StationName.En = stationNameEn;
-	}
-	public float getStationLon() {
-		return StationPosition.PositionLon;
-	}
-	public void setStationLon(float stationLon) {
-		StationPosition.PositionLon = stationLon;
-	}
-	public float getStationLat() {
-		return StationPosition.PositionLat;
-	}
-	public void setStationLat(float stationLat) {
-		StationPosition.PositionLat = stationLat;
-	}
-	public String getStationAddress() {
-		return StationAddress;
-	}
-	public void setStationAddress(String stationAddress) {
-		StationAddress = stationAddress;
-	}
-	public String getStationPhone() {
-		return StationPhone;
-	}
-	public void setStationPhone(String stationPhone) {
-		StationPhone = stationPhone;
-	}
-	public String getStationClass() {
-		return StationClass;
-	}
-	public void setStationClass(String stationClass) {
-		StationClass = stationClass;
-	}
+}
+
+class StationNameC
+{
+	String Zh_tw;
+	String En;
+}
+class StationPositionC
+{
+	float PositionLon;
+	float PositionLat;
 }
